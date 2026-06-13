6ddaaf3ed2a7eb26a715cbd286d5add56427d9b2
VANI20345 <aboody.sh.2003@gmail.com>
2026-06-13 10:03:31 +0300

    Updates

--- assets/triangle-alert-DXARJ0-G.js
+++ assets/triangle-alert-DXARJ0-G.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BaE0dJJC.js";/**
+import{c as e}from"./index-DMUhQh2l.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
